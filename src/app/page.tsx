dcf6d59ac43ae41d9bf6be60f97444a764a7c77b
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2025-08-20 19:55:28 +0530

    build error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading...</div>}> 
       <HomeClient />
     </Suspense>
   );
